44271d69407c1aa8cac5c95a701a1aadc914c958
Alejandro Ávila <159159333+alejandroavila17@users.noreply.github.com>
2024-03-21 14:27:42 -0400

    Se agregó el subContainer

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -15,40 +15,23 @@ const Login = () => {
         console.log("Chacon es un mmwebo");
     };
     return (
-        <div className="">
-            <h2>Login</h2>
-            <Link to="/facturacion">Facturacion</Link>
-
+        <div className="containerGeneral">
             <div className="container">
-                <InputGeneral
-                    type="text"
-                    placeholder="Correo Electrónico"
-                />
-                <InputGeneral
-                    type="text"
-                    placeholder="Contraseña"
-                />
-                <BtnGeneral
-                    text="Ingresar"
-                    handleClick={Click}
-                    width="110px"
-                    height="45px"
-                    color="#AEBBFD"
-                    img={image}
-                />
-                <BtnGeneral
-                    text="Chacon"
-                    handleClick={Chacon}
-                    width="200px"
-                    height="45px"
-                    color="red"
-                />
-                <BtnGeneral
-                    text="Test"
-                    handleClick={Click}
-                    img={image}
-                    color="#aebbfd"
-                />
+                <div className="subContainer">
+                    <InputGeneral
+                        type="text"
+                        placeholder="Correo Electrónico"
+                    />
+                    <InputGeneral type="text" placeholder="Contraseña" />
+                    <BtnGeneral
+                        text="Ingresar"
+                        handleClick={Click}
+                        width="110px"
+                        height="45px"
+                        color="#AEBBFD"
+                        img={image}
+                    />
+                </div>
             </div>
         </div>
     );
