d1f82077c242725993b7b4c955eb427814dd91f9
alejandroamesty <alejandro.30337890@uru.edu>
2024-03-24 18:00:41 -0400

    Merge request

--- src/components/navBar/View/Facturacion.jsx
+++ src/components/navBar/View/Facturacion.jsx
@@ -1,14 +1,19 @@
 import "./css/Facturacion.css";
-import React from "react";
+import React, { useState } from "react";
 import BtnGeneral from "../../../components/buttons/BtnGeneral";
 import InputDinamico from "../../../components/inputs/InputDinamico";
 import InputDiferente from "../../../components/inputs/InputDiferente";
 import svgAdd from "../../../assets/svg_add.svg";
 import svgSearch from "../../../assets/SearchSVG.svg";
 import cartSVG from "../../../assets/marketKart.svg";
+import ProductTable from "../../tables/productTable";
 
 const Facturacion = () => {
-	const monto = "0.00";
+	const [montoTotal, setMontoTotal] = useState("0.00");
+
+	const actualizarMontoTotal = (nuevoMonto) => {
+		setMontoTotal(nuevoMonto);
+	};
 
 	return (
 		<>
@@ -67,25 +72,13 @@ Prados del Este Piso 20 Oficina 20-06"
 				</div>
 
 				<div className="FacturaTableContainer">
-					{/* placeholder mientras hacemos la tabla */}
-					<div
-						style={{
-							width: "90%",
-							height: "85%",
-							alignSelf: "center",
-							backgroundColor: "white",
-							border: "1.42917px solid #000000",
-							boxSizing: "border-box",
-							boxShadow: "3.81111px 5.71667px 0px #000000",
-							borderRadius: "22.8667px",
-						}}
-					></div>
+					<ProductTable width="90%" height="85%" onTotalChange={actualizarMontoTotal} />
 				</div>
 
 				<div className="FacturaCheckoutContainer">
 					<div style={{ display: "flex", flexDirection: "column" }}>
 						<p style={{ position: "relative", marginLeft: "auto", fontSize: "18px" }}>Total:</p>
-						<p style={{ position: "relative", marginLeft: "auto", fontSize: "25.4331px", fontWeight: "bold" }}>$ {monto}</p>
+						<p style={{ position: "relative", marginLeft: "auto", fontSize: "25.4331px", fontWeight: "bold" }}>$ {montoTotal}</p>
 					</div>
 					<BtnGeneral text="Checkout" width="140px" color="#ff6060" onHoverColor="#c54444" img={cartSVG} />
 				</div>
