e077ee122e987606fa3e6aff7ebada5eaef51115
alejandroamesty <alejandro.30337890@uru.edu>
2024-03-27 11:14:02 -0400

    Se arregló el bug

--- src/components/containers/Producto.jsx
+++ src/components/containers/Producto.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import "./css/producto.css";
-const Producto = ({data}) =>{
+
+const Producto = ({ name, price }) => {
     return (
         <div className="card">
-            <div className="img-card">
-
-            </div>
-
-            <div className="add-cart">
-                Add to Cart
-            </div>
+            <div className="img-card"></div>
+            <div className="add-cart">Add to Cart</div>
             <div className="info-card">
-                {data.map((item) => (
-                        <div key={item.id}>
-                            <p><strong>{item.name}</strong></p>
-                            <p>Price : {item.price}$</p>
-                        </div>
-                    ))
-                }
+                <div>
+                    <p>
+                        <strong>{name}</strong>
+                    </p>
+                    <p>Price : {price}$</p>
+                </div>
             </div>
-      </div>
-    )
-}
+        </div>
+    );
+};
 
 export default Producto;
--- src/components/navBar/View/Productos.jsx
+++ src/components/navBar/View/Productos.jsx
@@ -1,72 +1,40 @@
-import React, {useState, useEffect} from "react";
-import Producto from "../../containers/Producto.jsx"
+import React, { useState, useEffect } from "react";
+import Producto from "../../containers/Producto.jsx";
 import "./css/Productos.css";
 
 const Productos = () => {
+    const [productos, setProductos] = useState([]);
 
-const product = [
-	{
-		id: 1,
-		name: "Producto 1",
-		price: 15,
-	},
-	{
-		id: 2,
-		name: "Producto 2",
-		price: 20,
-	},
-	{
-		id: 3,
-		name: "Producto 3",
-		price: 25,
-	},
-	{
-		id: 4,
-		name: "Producto 4",
-		price: 30,
-	},
-	{
-		id: 5,
-		name: "Producto 5",
-		price: 35,
-	},
-	{
-		id: 6,
-		name: "Producto 6",
-		price: 40,
-	},
-	{
-		id: 7,
-		name: "Producto 7",
-		price: 45,
-	},
-	{
-		id: 8,
-		name: "Producto 8",
-		price: 50,
-	},
-	{
-		id: 9,
-		name: "Producto 9",
-		price: 55,
-	},
-	{
-		id: 10,
-		name: "Producto 10",
-		price: 60,
-	},
-];
+    useEffect(() => {
+        fetch("/src/json/productos.json")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error fetching data");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const productosArray = Object.values(data);
+                setProductos(productosArray);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
 
-	return <>
-		<div className="containerMain">
-			<div className="containerCard">
-				{product.map((item) => (
-					<Producto key={item.id} data={item} />
-				))}
-			</div>
-			
-		</div>
-	</>;
+    return (
+        <div className="containerMain">
+            <div className="containerCard">
+                {productos.map((producto, index) => (
+                    <Producto
+                        key={index}
+                        name={producto.name}
+                        price={producto.price}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Productos;
