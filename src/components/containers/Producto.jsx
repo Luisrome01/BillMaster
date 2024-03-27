78c930412a8706c10436a64491e5f8194b896a38
Misa24jr <misa24jr@gmail.com>
2024-03-27 03:31:01 -0400

    Se emepzó a trabajar en la vista de productos

---
+++ src/components/containers/Producto.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./css/producto.css";
+const Producto = () =>{
+    return (
+        <div class="card">
+            <div class="img-card">
+
+            </div>
+
+            <div class="add-cart">
+                Add to Cart
+            </div>
+            <div class="info-card">
+                <p><strong>Lorem ipsum</strong></p>
+                <p>Price : 15$</p>
+            </div>
+      </div>
+    )
+}
+
+export default Producto;
--- src/components/navBar/View/Productos.jsx
+++ src/components/navBar/View/Productos.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import Producto from "../../containers/Producto.jsx"
+import "./css/Productos.css";
 const Productos = () => {
-	return <></>;
+	return <>
+		<div className="containerMain">
+			<div className="containerCard">
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+				<Producto />
+			</div>
+			
+		</div>
+	</>;
 };
 
 export default Productos;
